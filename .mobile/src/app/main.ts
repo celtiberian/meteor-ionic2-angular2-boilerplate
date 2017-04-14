aa6f8c46b281120049e2d551b3d1336d8f7f08d5
jlcastillo <jlcastillo@celtiberian.es>
2017-04-14 22:21:01 +0200

    mobile app removed from root folder

--- .mobile/src/app/main.ts
+++
@@ -1,7 +0,0 @@
-import 'meteor-client';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
